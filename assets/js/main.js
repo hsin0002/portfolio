cf09ad451d627b9a3985bb22a7a72fd0b0fdeca8
Hsin-Jou WU <tina3.14159265@gmail.com>
2025-10-09 00:49:26 +0200

    motivation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,123 +1,102 @@
 /*
 	Stellar by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Modified for sticky top navbar with scroll active links
 */
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$main = $('#main');
+	var $window = $(window),
+	    $body = $('body'),
+	    $main = $('#main'),
+	    $nav = $('#nav');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
+	// Breakpoints
+	breakpoints({
+		xlarge:   [ '1281px',  '1680px' ],
+		large:    [ '981px',   '1280px'  ],
+		medium:   [ '737px',   '980px'   ],
+		small:    [ '481px',   '736px'   ],
+		xsmall:   [ '361px',   '480px'   ],
+		xxsmall:  [ null,      '360px'   ]
+	});
+
+	// Play initial animations on page load
+	$window.on('load', function() {
+		setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
+	// Sticky Nav
+	if ($nav.length > 0) {
+
+		// 記錄 nav 原始偏移
+		var navOffsetTop = $nav.offset().top;
+
+		$window.on('scroll', function() {
+			if ($window.scrollTop() > navOffsetTop) {
+				$nav.addClass('fixed'); // 加上固定樣式
+			} else {
+				$nav.removeClass('fixed');
+			}
 		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
+		// Nav Links
+		var $nav_a = $nav.find('a');
+
+		$nav_a.scrolly({
+			speed: 1000,
+			offset: function() { return $nav.outerHeight(); } // 考慮 nav 高度
+		}).on('click', function() {
+
+			var $this = $(this);
+
+			// 外部連結? 直接返回
+			if ($this.attr('href').charAt(0) != '#')
+				return;
+
+			// 解除所有 active / active-locked
+			$nav_a.removeClass('active').removeClass('active-locked');
+
+			// 鎖定點擊的 link
+			$this.addClass('active').addClass('active-locked');
 		});
 
-	// Nav.
-		var $nav = $('#nav');
+		// Section scroll spy
+		$nav_a.each(function() {
+			var $this = $(this),
+			    id = $this.attr('href'),
+			    $section = $(id);
 
-		if ($nav.length > 0) {
+			if ($section.length < 1) return;
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+			$section.scrollex({
+				mode: 'middle',
+				initialize: function() {
+					if (browser.canUse('transition'))
+						$section.addClass('inactive');
+				},
+				enter: function() {
+					$section.removeClass('inactive');
 
-			// Links.
-				var $nav_a = $nav.find('a');
+					if ($nav_a.filter('.active-locked').length === 0) {
+						$nav_a.removeClass('active');
+						$this.addClass('active');
+					} else if ($this.hasClass('active-locked')) {
+						$this.removeClass('active-locked');
+					}
+				}
+			});
+		});
 
-				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
+	}
 
-						var $this = $(this);
-
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
-
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
-
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
-
-					})
-					.each(function() {
-
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
-
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
-
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
-
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
-
-								},
-								enter: function() {
-
-									// Activate section.
-										$section.removeClass('inactive');
-
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
-
-											$nav_a.removeClass('active');
-											$this.addClass('active');
-
-										}
-
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
-
-								}
-							});
-
-					});
-
+	// Smooth Scrolling
+	$('.scrolly').scrolly({
+		speed: 1000,
+		offset: function() {
+			return $nav.outerHeight();
 		}
-
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000
-		});
+	});
 
 })(jQuery);
